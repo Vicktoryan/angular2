25bebc811f452e5090ef322af00c44ac925e516b
Alex <alien000@mail.ru>
2018-11-24 04:51:21 +0600

    Add login module

---
+++ task1/src/app/services/alert.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {Subject, Observable} from "rxjs";
+import {IAlert} from "../interfaces/IAlert";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  private subject = new Subject<any>();
+  private alertList: IAlert[] = [];
+
+  constructor() {
+
+  }
+
+  setAlert(alert: IAlert, timeLife: number = null): void {
+    if (!this.alertList.some((item: IAlert) => item.message === alert.message)) {
+      this.alertList.push(alert);
+      if (alert.type !== 'error' || timeLife) {
+        setTimeout(() => {
+          this.alertList.splice(this.alertList.indexOf(alert), 1);
+          this.subject.next(this.alertList);
+        }, timeLife || 5000);
+      }
+      this.subject.next(this.alertList);
+    }
+  }
+
+  clearAlert(): void {
+    this.alertList = [];
+    this.subject.next();
+  }
+
+  getAlert(): Observable<any> {
+    return this.subject.asObservable();
+  }
+
+  removeAlert(alert: IAlert): void {
+    this.alertList.splice(this.alertList.indexOf(alert), 1);
+    this.subject.next(this.alertList);
+  }
+}
---
+++ task1/src/app/services/common.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {IUserInformation} from "../interfaces/IUserInformation";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+  private userInformation: IUserInformation;
+  constructor() {
+
+  }
+
+  public login(login: string, password: string) {
+    const promise = new Promise((resolve) => {
+      if (login === '1' && password === '1') {
+        this.setUserInformation();
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+    return promise;
+  }
+
+  public getUserInformation(): IUserInformation {
+    return this.userInformation;
+  }
+
+  private setUserInformation(): void {
+    this.userInformation = {
+      sessionId: '1',
+      userName: 'Test name'
+    };
+  }
+}
